a6b9c657bc02d76cb343d5e6f5e7817bf48a8ce9
jhjeon <jhjeon@eyedea.co.kr>
2018-05-15 02:02:44 -0700

    11052

---
+++ algorithm/dp/11052/11052.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<algorithm>
+
+using namespace std;
+int p[1001];
+int result[1001];
+int f(int co){
+	
+}
+
+int main(){
+	int N;
+	
+	scanf("%d",&N);
+	for(int i = 0 ; i < N ; i++){
+		scanf("%d",&p[i]);
+	}
+	printf("%d",f(N));
+	return 0;
+
+}
