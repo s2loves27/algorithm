822ab6130a83673c830f5556d45577126a754d28
jhjeon <jhjeon@eyedea.co.kr>
2018-06-11 01:30:22 -0700

    solve 4256

--- 4000/4200/4256/4256.cpp
+++ 4000/4200/4256/4256.cpp
@@ -7,41 +7,34 @@ using namespace std;
 int T,n;
 int preorder[1001];
 int inorder[1001];
-int postorder[1001];
+int inpos[1001];
 int ch = 0;
-int f(int start , int end,int ch){
-	if(start+1 == end)  {return inorder[start];}
-	for(int i = start ; i < end ; i++){
-		
-			if(preorder[ch] == inorder[i]){
-				printf("%d ",f(start,i,start+1));
-				printf("%d ",f(i,end,i+1));
+void f(int start , int end){
+	if(start == end)  {return;}
+	int root = preorder[ch++];
+	int i = inpos[root];
+	f(start,i);
+	f(i+1,end);
+	printf("%d ",root+1);		
 
-				//f(start,i);
-				//f(i,end);
-				printf("%d ",inorder[i]);
-				return 0;
-			}
-		}
 }
-void print(){
-	for(int i = 0 ; i < n ; i++){
-		printf("%d ",postorder[i]);
-	}
-	printf("\n");
-}
+
 int main(){
 	scanf("%d",&T);
 	for(int i = 0 ; i < T ; i++){
+		ch = 0;
 		scanf("%d",&n);
 		for(int j = 0 ; j < n ; j++ ){
 			scanf("%d",&preorder[j]);
+			preorder[j]--;
 		}
 		for(int j = 0 ; j < n ; j++ ){
 			scanf("%d",&inorder[j]);
+			inorder[j]--;
+			inpos[inorder[j]] = j; 
 		}
-		f(0,n,0);
-		//print();
+		f(0,n);
+		printf("\n");
 	}
 	return 0 ;
 }
